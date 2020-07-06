03dcc6cb2126dab7e1477a6017bb34fb55f2dea5
waldganger <antony.merle@yahoo.fr>
2020-07-06 14:58:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let a = prompt("First number ?", 1);
+let b = prompt("First number ?", 2);
+
+alert(+a + +b);
+
+
+// let nom = prompt("Entrez votre nom", "ici");
+// let age = 2**8;
+
+// alert(`Bonjour ${nom}, vous avez ${age} ans.`);
