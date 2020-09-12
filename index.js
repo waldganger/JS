d98aff44e167757b3ff17f40db62802385438b65
waldganger <antony.merle@yahoo.fr>
2020-09-12 23:13:41 +0200

    dominant writing direction

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 'use strict'
 
+const SCRIPTS = require('./scripts.js');
 
 
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let direction = groupName(item);
+    let known = counts.findIndex(c => c.direction == direction);
+    if (known == -1) {
+      counts.push({direction, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+
+function dominantDirection(text){
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    // if (script != null) console.log(script.direction);
+    return script ? script.direction : "none";
+  }).filter(({direction}) => direction != "none");
+
+  let total = scripts.reduce((sum, {count}) => sum + count, 0);
+  if (total === 0) return "No scripts found";
+
+  // console.log(scripts);
+  return scripts.reduce((a, b) => {
+   return a > b ? a : b;
+  }).direction;
+
+  // return scripts.map(({direction, count}) => {
+  //   return `${Math.round(count * 100 / total)}% ${direction}`;
+  // }).join(", ");
+}
+
+// console.log(dominantDirection('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+console.log(dominantDirection("Hello!"));
+
+console.log(dominantDirection("Hey, مساء الخير"));
+
+
+// countBy(texte, dominantDir)
 
 // let array = [1, 2, 3, 4, 5];
 
