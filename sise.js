15c5e3565516edb43932a48aec5f80cd84c7bfd6
waldganger <antony.merle@yahoo.fr>
2020-09-26 22:52:01 +0200

    fn listeSite & listeProperty ok

--- sise.js
+++ sise.js
@@ -15,9 +15,25 @@ function fieldList(data){
     return set;
 }
 
-function disciplinesList(data){
+function listeDisciplines(data){
     let set = new Set;
     Object.values(data).map(el => set.add(el.fields.sect_disciplinaire_lib));
+    return Array.from(set);
+}
+
+function listeSite(data) {
+    let set = new Set;
+    Object.values(data).map(el => set.add(el.fields.com_ins_lib));
+    return Array.from(set);
+}
+
+// faire la liste de toutes les proprietes
+// à la demande passer chaque propriété à une fn comme fteDiscipline pour obtenir les fte
+// afficher avec une fonction comme printFteByDiscipline
+
+function listeProperty(data, property){
+    let set = new Set;
+    Object.values(data).map(el => set.add(el.fields[property]));
     return Array.from(set);
 }
 
@@ -29,7 +45,7 @@ function fteDiscipline(data, disc){
 
 function printFteByDiscipline(data){
     console.table(
-        disciplinesList(data).map(disc => {
+        listeDisciplines(data).map(disc => {
             return {
                 "Discipline": disc,
                 "FTE": fteDiscipline(data, disc)
@@ -37,16 +53,17 @@ function printFteByDiscipline(data){
         }));
 }
 
-printFteByDiscipline(json);
 
 function totalFte(data){
-    
-    return disciplinesList(data).map(disc => fteDiscipline(data, disc)).reduce((sum, current) => sum + current, 0);
+    return listeDisciplines(data).map(disc => fteDiscipline(data, disc)).reduce((sum, current) => sum + current, 0);
 }
 
 // TESTS
+console.log(listeProperty(json, "com_ins_lib"));
+// console.log(listeSite(json));
+// printFteByDiscipline(json);
 // console.log(totalFte(json) === fte(json));
 // console.log(totalFte(json));
 // console.log( fteDiscipline(json, 'Informatique') );
 // Object.values(json).forEach(el => console.log(el));
-// console.log(disciplinesList(json));
+// console.log(listeDisciplines(json));
